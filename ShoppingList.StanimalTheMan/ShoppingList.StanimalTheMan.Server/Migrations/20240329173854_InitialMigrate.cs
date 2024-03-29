a732646a736ea834f18b62219435aa9ad097b0cd
Stan Choi <sjc591@nyu.edu>
2024-03-29 13:44:39 -0400

    Initialize app and create ShoppingListItem, ShoppingListItemDbContext for initial migration of data model

---
+++ ShoppingList.StanimalTheMan/ShoppingList.StanimalTheMan.Server/Migrations/20240329173854_InitialMigrate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingList.StanimalTheMan.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IsPickedUp = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingListItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingListItems");
+        }
+    }
+}
---
+++ ShoppingList.StanimalTheMan/ShoppingList.StanimalTheMan.Server/Models/ShoppingListItemDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingList.StanimalTheMan.Server.Models;
+
+public class ShoppingListItemDbContext : DbContext
+{
+	public DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Server=(LocalDb)\\LocalDBDemo;Database=ShoppingListItems;Trusted_Connection=True;");
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<ShoppingListItem>()
+			.Property(x => x.IsPickedUp)
+			.HasDefaultValue(false);
+
+		base.OnModelCreating(modelBuilder);
+	}
+}
