d45b89de71c6ae2f32042677b7c8e23d2ddcfe34
Stevie Robinson <stevie.robinson@gmail.com>
2024-05-28 22:21:12 +0200

    initial creation of model, context and controller

---
+++ ShoppingList.StevieTV/ShoppingList.StevieTV/Models/ShoppingListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingList.StevieTV.Models;
+
+public class ShoppingListItem
+{
+    public int Id { get; set; }
+    public string Item { get; set; }
+    public bool IsPickedUp { get; set; }
+}
---
+++ ShoppingList.StevieTV/ShoppingList.StevieTV/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.StevieTV.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ShoppingListContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShoppingListContext") ?? throw new InvalidOperationException("Connection String not found")));
+
+builder.Services.AddScoped<ShoppingListContext>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.Services.CreateScope().ServiceProvider.GetService<ShoppingListContext>().Database.EnsureCreated();
+}
+
+app.UseCors(options => 
+    options.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+);
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
