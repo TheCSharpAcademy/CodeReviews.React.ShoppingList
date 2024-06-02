6ef1c741bf745eb2a898d4e1ea0ea1b68eed3ffe
Stevie Robinson <stevie.robinson@gmail.com>
2024-06-02 12:26:45 +0200

    update CORS headers for dev and preview options of Vite

--- ShoppingList.StevieTV/ShoppingList.StevieTV/Program.cs
+++ ShoppingList.StevieTV/ShoppingList.StevieTV/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(options => 
-    options.WithOrigins("http://localhost:5173")
+    options.WithOrigins("http://localhost:5173", "http://localhost:4173")
         .AllowAnyMethod()
         .AllowAnyHeader()
 );
