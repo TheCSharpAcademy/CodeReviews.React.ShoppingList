6b23453decc98cc532a641c037a7717e079aed0c
Sebastian <mrseb10@gmail.com>
2024-06-29 23:20:54 +0200

    Project Completed

---
+++ ShoppingList.BBualdo/Infrastructure/Repositories/IProductsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Repositories;
+
+public interface IProductsRepository
+{
+  Task<PaginatedProducts> GetProductsAsync(int page, int pageSize);
+  Task<Product?> GetProductByIdAsync(int id);
+  Task AddProductAsync(Product product);
+  Task UpdateProductAsync(Product product);
+  Task DeleteProductAsync(Product product);
+}
---
+++ ShoppingList.BBualdo/Infrastructure/Services/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+
+public interface IProductsService
+{
+  Task<PaginatedProducts> GetAsync(int page, int pageSize);
+  Task AddAsync(Product product);
+  Task UpdateAsync(Product product);
+  Task DeleteAsync(int id);
+}
